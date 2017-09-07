00389bbc76f5b3f0773cd3ce8d615aaaf120f82c
Ubuntu <ubuntu@ip-172-31-9-98.us-west-2.compute.internal>
2017-09-07 06:21:34 +0000

    Need to rebuild scorecheck

--- scorecheck.py
+++ scorecheck.py
@@ -41,87 +41,73 @@ def launchSkill():
             convert = {'user_date': 'date'})
 def scoreCheck(user_date, team):
     try:
-        if isinstance(user_date, datetime.date) is False:
-           '''Tests whether the user_date is a valid date'''
-               bad_date = render_template('bad_date')
-                return question(bad_date)
+        if len(str(user_date)) == 0:
+            '''Captures missing date requests'''
+            bad_date = render_template('bad_date')
+            return question(bad_date)
 
-            elif team not in team_list:
-                '''Tests whether an accurate team name was given'''
-                bad_team = render_template('bad_team')
-                return question(bad_team)
+        elif isinstance(user_date, datetime.date) is not True:
+            '''Tests whether the user_date is a valid date'''
+            bad_date = render_template('bad_date')
+            return question(bad_date)
 
-            else:
-                '''The scoreCheck() function accepts a team name and spoken date from the user, converts the month name to a
-                number (because mlbgame requires integer dates), and returns the game score.'''
-                _year = user_date.year
-                _month = user_date.month
-                _day = user_date.day
+        elif team.title() not in team_list:
+            '''Tests whether an accurate team name was given'''
+            bad_team = render_template('bad_team')
+            return question(bad_team)
 
-                '''Team names are case sensitive (White Sox, not White sox; Tigers, not tigers).'''
-                team = team.title()
+        else:
+            '''The scoreCheck() function accepts a team name and spoken date from the user, converts the month name to a
+            number (because mlbgame requires integer dates), and returns the game score.'''
+            _year = user_date.year
+            _month = user_date.month
+            _day = user_date.day
 
-                global games
-                games = mlbgame.day(_year, _month, _day, away=team, home=team)
+            global games
+            games = mlbgame.day(_year, _month, _day, away=team.title(), home=team.title())
+            win_team = games[0].w_team
+            win_pitcher = 'The winning pitcher was %s of the %s.' % (games[0].w_pitcher, games[0].w_team)
+            for game in games:
+                if len(games) == 0:
+                    '''mlbgame() returns data in a dictionary. If there is no game, the dictionary will be empty.'''
+                    no_game_look_up = render_template('no_game_look_up')
+                    return question(no_game_look_up)
+                elif win_team == team.title():
+                    game_info = 'The ' + team.title() + ' won. The final score was ' + str(game) + '. The winning pitcher was ' + str(games[0].w_pitcher) + ' of the ' + str(games[0].w_team) + '.'
+                    return question(game_info)
+                elif win_team != team.title():
+                    game_info = 'The ' + team.title() + ' lost. The final score was ' + str(game) + '. The winning pitcher was ' + str(games[0].w_pitcher) + ' of the ' + str(games[0].w_team) + '.'
+                    return question(game_info)
 
-                try:
-                    if len(games) == 0:
-                        '''mlbgame() returns data in a dictionary. If there is no game, the dictionary will be empty.'''
-                        no_game_look_up = render_template('no_game_look_up')
-                        return question(no_game_look_up)
-                    else:
-                        win_team = games[0].w_team
-                        win_pitcher = 'The winning pitcher was %s of the %s.' % (games[0].w_pitcher, games[0].w_team)
-                        for game in games:
-                            if win_team == team:
-                                game_info = 'The ' + team + ' won. The final score was ' + str(game) + '. The winning pitcher was ' + str(games[0].w_pitcher) + ' of the ' + str(games[0].w_team) + '.'
-                                return question(game_info)
-                            elif win_team != team:
-                                game_info = 'The ' + team + ' lost. The final score was ' + str(game) + '. The winning pitcher was ' + str(games[0].w_pitcher) + ' of the ' + str(games[0].w_team) + '.'
-                                return question(game_info)
-                except OSError:
-                    '''Returned if the user asked for a date before 2005'''
-                    out_of_date = render_template('out_of_date')
-                    return question(out_of_date)
+    except OSError:
+        '''Returned if the user asked for a date before 2005'''
+        out_of_date = render_template('out_of_date')
+        return question(out_of_date)
 
-                except IndexError:
-                    '''Rarely returned if the user asked for a date before 2005'''
-                    out_of_date = render_template('out_of_date')
-                    return question(out_of_date)
+    except IndexError:
+        '''Rarely returned if the user asked for a date before 2005'''
+        out_of_date = render_template('out_of_date')
+        return question(out_of_date)
 
-                except AttributeError:
-                    '''Returned if the user asked for a nonsensical date ('the day before six weeks ago tomorrow') '''
-                    bad_date = render_template('bad_date')
-                    return question(bad_date)
+    except AttributeError:
+        '''Returned if the user asked for a nonsensical date ('the day before six weeks ago tomorrow') '''
+        bad_date = render_template('bad_date')
+        return question(bad_date)
 
 @ask.intent("teamList")
 def teamList():
     team_list = render_template('team_list')
     return question(team_list)
 
+@ask.intent("stopCancel")
+def stopCancel():
+    stop_cancel = render_template('stop_cancel')
+    return statement(stop_cancel)
+
 @ask.intent("greetingHelp")
 def greetingHelp():
     greeting_help = render_template('greeting_help')
     return question(greeting_help)
-
-@ask.intent("stop")
-def stop():
-    stop = render_template('stop_cancel')
-    return statement(stop)
-
-@ask.intent("cancel")
-def cancel():
-    cancel = render_template('stop_cancel')
-    return statement(cancel)
-
-@ask.intent("stop_cancel")
-def stop_cancel():
-    stop_cancel = render_template('stop_cancel')
-    return statement(stop_cancel)
-
-@ask.session_ended
-def session_ended():
-    return "{}", 200
 
 #### Main
 if __name__ == "__main__":
